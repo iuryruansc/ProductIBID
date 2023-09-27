96bcf857c238ff6a40239f3d255bbbdc59f75320
iuryruansc <iuryruansc@gmail.com>
2023-09-27 17:26:09 -0300

    criando o construtor do objeto produto

---
+++ Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductIBID
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public Produto()
+        {
+            Nome = "";
+        }
+    }
+}
